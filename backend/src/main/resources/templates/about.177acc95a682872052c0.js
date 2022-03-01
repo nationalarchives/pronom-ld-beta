d973d7c5ae30561cbcbca70541c1dad2ad6089f6
afduarte <a.fduarte1@gmail.com>
2022-03-01 14:20:37 +0000

    ignoring resource template files

--- backend/src/main/resources/templates/about.177acc95a682872052c0.js
+++
@@ -1,2 +0,0 @@
-(()=>{"use strict";console.log("about page JS"),console.log("JS is running")})();
-//# sourceMappingURL=about.177acc95a682872052c0.js.map
--- backend/src/main/resources/templates/index.177acc95a682872052c0.js
+++
@@ -1,2 +0,0 @@
-(()=>{"use strict";console.log("index page JS"),console.log("JS is running")})();
-//# sourceMappingURL=index.177acc95a682872052c0.js.map
