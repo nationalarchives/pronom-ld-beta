1b70349e2d93ec85dd431cca23093b5ae1fd596a
afduarte <a.fduarte1@gmail.com>
2022-03-02 13:58:36 +0000

    Configuring assembly to copy files to backend dir

--- frontend/assemble.config.js
+++ frontend/assemble.config.js
@@ -1,11 +1,32 @@
 const path = require('path');
 const { merge } = require('webpack-merge');
 const prod = require('./prod.config.js');
-const buildPath = path.resolve(__dirname, '../backend/src/main/resources/templates/');
+const resourcesDir = path.resolve(__dirname, '../backend/src/main/resources');
+const FileManagerPlugin = require('filemanager-webpack-plugin');
 
 // Same as Production build but outputs to the backend project so the JAR can be built
 module.exports = merge(prod, {
-  output: {
-    path: buildPath,
-  }
+  plugins: [
+    new FileManagerPlugin({
+      events: {
+        onEnd: [
+          {
+            delete: [
+              { source: `${resourcesDir}/static`, options: { force: true } },
+              { source: `${resourcesDir}/templates`, options: { force: true } }
+            ]
+          },
+          { mkdir: [`${resourcesDir}/static/media`, `${resourcesDir}/templates`] },
+          {
+            copy: [
+              { source: `./dist/*.!(html)`, destination: `${resourcesDir}/static/` },
+              { source: `./dist/static/media/*`, destination: `${resourcesDir}/static/media/` },
+              { source: `./dist/*.html`, destination: `${resourcesDir}/templates/` },
+            ]
+          },
+        ],
+      },
+    })
+  ]
 });
+
