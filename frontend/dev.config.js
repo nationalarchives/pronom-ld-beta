c590676964c6ed1032d045ee3b472eda1b3b7786
afduarte <a.fduarte1@gmail.com>
2022-03-03 14:56:47 +0000

    Making links with no html extension work on local dev

--- frontend/dev.config.js
+++ frontend/dev.config.js
@@ -1,5 +1,15 @@
 const { merge } = require('webpack-merge');
+const fs = require('fs');
+const path = require('path');
 const common = require('./common.config.js');
+const SRC = path.resolve(__dirname, 'src');
+
+const HTMLFiles = fs.readdirSync(SRC + '/base').filter(file => file.includes('.pug')).map(file => {
+  const [name, ext] = file.split('.');
+  return {
+    from: new RegExp(`^\/${name}$`), to: `/${name}.html`,
+  }
+});
 
 module.exports = merge(common, {
   mode: 'development',
@@ -8,5 +18,14 @@ module.exports = merge(common, {
     static: 'src/base',
     port: 8080,
     magicHtml: true,
+    client: {
+      logging: 'verbose',
+    },
+    historyApiFallback: {
+      rewrites:[
+        { from: /^\/$/, to: '/index.html' },
+        ...HTMLFiles,
+      ]
+    }
   }
 });
