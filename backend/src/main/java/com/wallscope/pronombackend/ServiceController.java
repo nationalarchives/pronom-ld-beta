383df2b04e2c811a6a49ba0a1281577c5fe02f8d
afduarte <a.fduarte1@gmail.com>
2022-03-25 12:56:10 +0000

    Changing service controller to be REST controller

--- backend/src/main/java/com/wallscope/pronombackend/ServiceController.java
+++ backend/src/main/java/com/wallscope/pronombackend/ServiceController.java
@@ -1,13 +1,13 @@
 package com.wallscope.pronombackend;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@Controller
+@RestController
 public class ServiceController {
     @GetMapping("/health")
     public Map<String, String> health(Model model) {
