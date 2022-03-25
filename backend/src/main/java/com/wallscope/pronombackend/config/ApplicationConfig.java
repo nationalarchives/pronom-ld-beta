0860608244d2da09110c21a8c7a00d0242f7dc47
afduarte <a.fduarte1@gmail.com>
2022-03-25 12:56:24 +0000

    Fixing applicaiton config reading env vars

--- backend/src/main/java/com/wallscope/pronombackend/config/ApplicationConfig.java
+++ backend/src/main/java/com/wallscope/pronombackend/config/ApplicationConfig.java
@@ -3,7 +3,7 @@ package com.wallscope.pronombackend.config;
 public class ApplicationConfig {
     static String getEnvOr(String env, String or) {
         String v = System.getenv(env);
-        return v.equals("") ? or : v;
+        return v == null ? or : v;
     }
 
     public static String MARKDOWN_DIR = getEnvOr("MARKDOWN_DIR","md");
