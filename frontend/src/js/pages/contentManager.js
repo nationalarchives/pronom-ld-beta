e774c18112fbc3e4128e81a0940a84e79beaa629
DorotaBurdach <dorota.burdach555@gmail.com>
2022-03-29 11:49:28 +0100

    internal interface - menu + pages structure

---
+++ frontend/src/js/pages/contentManager.js
@@ -0,0 +1,9 @@
+import { test } from '@js/lib/test'
+
+import '@styles/main.scss'
+import '@styles/contentManager.scss'
+
+const App = () => {
+}
+
+App()
---
+++ frontend/src/js/pages/internalSearch.js
@@ -0,0 +1,9 @@
+import { test } from '@js/lib/test'
+
+import '@styles/main.scss'
+import '@styles/internalSearch.scss'
+
+const App = () => {
+}
+
+App()
