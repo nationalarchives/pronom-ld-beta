8963d5fd5051df470402eaeb62d7c03b857d452c
afduarte <a.fduarte1@gmail.com>
2022-03-29 10:56:19 +0100

    Adding logging capabilities

--- backend/src/main/java/com/wallscope/pronombackend/ServiceController.java
+++ backend/src/main/java/com/wallscope/pronombackend/ServiceController.java
@@ -1,5 +1,7 @@
 package com.wallscope.pronombackend;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +11,8 @@ import java.util.Map;
 
 @RestController
 public class ServiceController {
+    Logger logger = LoggerFactory.getLogger(ServiceController.class);
+
     @GetMapping("/health")
     public Map<String, String> health(Model model) {
         Map<String, String> res = new HashMap<>();
--- backend/src/main/java/com/wallscope/pronombackend/utils/TemplateUtils.java
+++ backend/src/main/java/com/wallscope/pronombackend/utils/TemplateUtils.java
@@ -1,23 +1,39 @@
 package com.wallscope.pronombackend.utils;
 
 import com.github.rjeschke.txtmark.Processor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 
 public class TemplateUtils {
+    Logger logger = LoggerFactory.getLogger(TemplateUtils.class);
+
     private final String mdDir;
 
     public TemplateUtils(String mdDir) {
         this.mdDir = mdDir;
     }
 
-    public String md(String section) {
+    public String md(String region) {
         try {
-            File f = new File(mdDir, section + ".md");
+            File f = new File(mdDir, region + ".md");
             return Processor.process(f);
         } catch (IOException e) {
-            e.printStackTrace();
+            logger.info("MD LOADER: No template for region: " + region);
+            return "";
+        }
+    }
+
+    public String raw(String region) {
+        try {
+            File f = new File(mdDir, region + ".md");
+            return Files.readString(f.toPath(), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            logger.info("RAW LOADER: No template for region: " + region);
             return "";
         }
     }
