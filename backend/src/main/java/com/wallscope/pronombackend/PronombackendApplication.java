55000dd3ea3aad58208369f92ba2588dc8ffe71e
afduarte <a.fduarte1@gmail.com>
2022-03-29 10:56:19 +0100

    Adding logging capabilities

--- backend/src/main/java/com/wallscope/pronombackend/PronombackendApplication.java
+++ backend/src/main/java/com/wallscope/pronombackend/PronombackendApplication.java
@@ -6,20 +6,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class PronombackendApplication {
 
-	@Bean(name = "templateUtils")
-	public TemplateUtils templateUtils(){
-		return new TemplateUtils(ApplicationConfig.MARKDOWN_DIR);
-	}
+    @Bean(name = "templateUtils")
+    public TemplateUtils templateUtils() {
+        return new TemplateUtils(ApplicationConfig.MARKDOWN_DIR);
+    }
 
 
-	public static void main(String[] args) {
-		ApplicationContext ctx = SpringApplication.run(PronombackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(PronombackendApplication.class, args);
+    }
 }
 
--- backend/src/main/java/com/wallscope/pronombackend/ReleaseNotesController.java
+++ backend/src/main/java/com/wallscope/pronombackend/ReleaseNotesController.java
@@ -1,16 +1,19 @@
 package com.wallscope.pronombackend;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 /*
-* This controller handles all calls to pages where the content does not depend on fetching data from the database
-* Hence the name static. The actual content may change based on the Markdown workflow
-* */
+ * This controller handles all calls to pages where the content does not depend on fetching data from the database
+ * Hence the name static. The actual content may change based on the Markdown workflow
+ * */
 @Controller
 public class ReleaseNotesController {
+    Logger logger = LoggerFactory.getLogger(ReleaseNotesController.class);
 
     @GetMapping("/release-notes/{version}")
     public String relNotes(Model model, @PathVariable(required = false) String version) {
