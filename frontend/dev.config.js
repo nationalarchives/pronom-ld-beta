1bbd65261c2f7911ce5576a661fa2e9453133537
DorotaBurdach <dorota.burdach555@gmail.com>
2022-03-31 19:04:35 +0100

    form segments for internal interface

--- frontend/dev.config.js
+++ frontend/dev.config.js
@@ -34,6 +34,7 @@ const rewrites = [
   { from: new RegExp("^\/dashboard$"), to: "/dashboard.html" },
   { from: new RegExp("^\/internal-search$"), to: "/internal-search.html" },
   { from: new RegExp("^\/content-manager$"), to: "/content-manager.html" },
+  { from: new RegExp("^\/internal-form$"), to: "/internal-form.html" },
 ]
 
 module.exports = merge(common, {
