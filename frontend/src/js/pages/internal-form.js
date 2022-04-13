dd93fd5289463484884f9cca3c47ad4db2aeff62
Dorota Burdach <dorotaburdach@Dorotas-MacBook-Pro.local>
2022-04-13 15:51:43 +0100

    fixing FAQ acordions to open multiple at once

--- frontend/src/js/pages/internal-form.js
+++ frontend/src/js/pages/internal-form.js
@@ -2,9 +2,18 @@ import '@styles/main.scss'
 import '@styles/user-form.scss'
 import '@styles/internal-form.scss'
 
+const formParts = ['#core', '#signatures', '#relationships', '#identifiers', '#additionalProperties', '#contributors', '#review']
+const formMenuButtons = ['#coreBtn', '#signaturesBtn', '#relationshipsBtn', '#identifiersBtn', '#additionalPropertiesBtn', '#contributorBtn', '#reviewBtn']
 const App = () => {
     // Signal JS is active
     $('.page-container').removeClass('noJS')
+    // Initialise form
+    let formStep = 6
+  
+    // active class to the button
+    $(formMenuButtons[formStep]).addClass( "active" );
+    // Show first step
+    $(formParts[formStep]).addClass('show');
 }
 
 App()
