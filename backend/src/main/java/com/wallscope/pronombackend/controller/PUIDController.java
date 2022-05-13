4dbb6f69f2da6322916286c4b86d6c8e555de015
afduarte <a.fduarte1@gmail.com>
2022-05-13 16:47:20 +0100

    Adding XML request parameter for dev signature to be included when requested

--- backend/src/main/java/com/wallscope/pronombackend/controller/PUIDController.java
+++ backend/src/main/java/com/wallscope/pronombackend/controller/PUIDController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.server.ResponseStatusException;
 
 import javax.servlet.http.HttpServletRequest;
@@ -50,10 +51,11 @@ public class PUIDController {
     }
 
     @GetMapping(value = {"/signature.xml"}, produces = "text/xml")
-    public String xmlAllSignatureHandler(Model model) {
+    public String xmlAllSignatureHandler(Model model, @RequestParam(required = false) Boolean dev) {
         FileFormatDAO dao = new FileFormatDAO();
         List<FileFormat> fs = dao.getAll();
         model.addAttribute("formats", fs);
+        model.addAttribute("dev", dev);
         return "xml_signatures";
     }
 
