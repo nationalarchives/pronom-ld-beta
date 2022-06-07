3f740847f8ba2bfde57086aae30d8ce18f430e2c
DorotaBurdach <dorota.burdach555@gmail.com>
2022-06-07 16:32:22 +0100

    feedback automatic height and top

--- frontend/src/js/pages/form-choice.js
+++ frontend/src/js/pages/form-choice.js
@@ -2,9 +2,12 @@ import '@styles/main.scss'
 import '@styles/form-choice.scss'
 
 const App = () => {
-    // Signal JS is active
-    $('.page-container').removeClass('noJS');
-    $('#header').removeClass('noJS');
+  // Signal JS is active
+  $('.page-container').removeClass('noJS');
+  $('#header').removeClass('noJS');
+	let feedbackBarHight = $('.feedback-inner').height();
+	$('.feedback-container').css('top','calc(100vh - '+ (feedbackBarHight + 35) +'px)');
+	$('.feedback-container').css('height','auto');
 }
 
 App()
