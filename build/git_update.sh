940213116cc28714df7102756dcd42f61bda1543
MichalHWallscope <michal.hoinca@wallscope.co.uk>
2022-07-15 16:17:34 +0100

    tagging test

--- build/git_update.sh
+++ build/git_update.sh
@@ -16,7 +16,7 @@ CURRENT_VERSION=`git describe --abbrev=0 --tags 2>/dev/null`
 
 if [[ $CURRENT_VERSION == '' ]]
 then
-  CURRENT_VERSION='v0.0.5'
+  CURRENT_VERSION='0.0.5'
 fi
 echo "Current Version: $CURRENT_VERSION"
 
